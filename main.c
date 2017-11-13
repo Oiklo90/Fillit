81dad8d81bf691e06cc18c36f88af0a3c4ea5f28
Loic LONGER <llonger@e2r12p6.42.fr>
2017-11-13 09:42:35 +0100

    check errors working

---
+++ main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llonger <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/10 13:59:27 by llonger           #+#    #+#             */
+/*   Updated: 2017/11/11 16:07:13 by llonger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "./libft/libft.h"
+#include "fillit.h"
+
+void	ft_error(void)
+{
+	ft_putendl("error");
+	exit(EXIT_FAILURE);
+}
+
+int		main(int ac, char **av)
+{
+	int		fd;
+	char	**tetri;
+	char	*sol;
+
+	if (ac != 2)
+	{
+		ft_putendl("usage: ./fillit <file_name> \n \
+	you need one file_name to run fillit, not zero, not two, \
+not 1 000000000000!!!");
+		return (0);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		ft_error();
+	if ((tetri = ft_read_file(fd)) == NULL)
+		ft_error();
+	//if ((sol = ft_resolve(tetri)) == NULL)
+	//	ft_error();
+	return 0;
+}
